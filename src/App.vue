85d45c24fec82f5346ac9120ba83b2bda34d920d
cksghkd123 <cksghkd1234@naver.com>
2022-06-25 23:58:07 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <CommonHeader />
+  <router-view />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import CommonHeader from "@/components/common/CommonHeader.vue";
+
+export default defineComponent({
+  components: {
+    CommonHeader,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/components/common/CommonHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header-wrapper">
+    <span class="name">ㄴㄴㄴ</span>
+    <div></div>
+    <button class="login-button">로그인</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.header-wrapper {
+  display: flex;
+  /* 축정렬 */
+  justify-content: space-between;
+  /* 축수직방향 정렬 */
+  align-items: center;
+  height: 70px;
+  background-color: blueviolet;
+  padding: 0 20px;
+}
+.login-button {
+  width: 100px;
+  height: 36px;
+  background: #abc234;
+  color: blue;
+  border: none;
+  border-radius: 10px;
+}
+.login-button:hover {
+  cursor: pointer;
+  background-color: bisque;
+}
+.name {
+  /* fontsize는 px말고 rem으로 해보자 */
+  font-size: 1.5rem;
+  color: aqua;
+  font-weight: 700;
+}
+</style>
