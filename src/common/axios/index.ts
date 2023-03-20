3d6741618d177bf3a0d0d1981c44437159103d9d
cksghkd123 <cksghkd1234@naver.com>
2023-03-20 22:42:04 +0900

    store 이용 axios로 서버와 통신

--- src/common/axios/index.ts
+++ src/common/axios/index.ts
@@ -1,7 +1,23 @@
+import store from "@/store"
 import axios from "axios"
 
 const client = axios.create({
-    baseURL: 'http://localhost:9000'
+    baseURL: 'http://localhost:9000',
 })
+
+client.interceptors.request.use(
+    async (config) => {
+        const token = store.state.token;
+
+        // 토큰이 있으면 Authorization 헤더에 추가
+        if (token) {
+            config.headers!.Authorization = 'Bearer ${token}';
+        }
+
+        return config
+    }, (error) => {
+        return Promise.reject(error)
+    })
+
 
 export default client
